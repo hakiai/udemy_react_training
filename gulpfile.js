afb24d568b716154f0406466263dc17d7e7f05af
hakiai <chu_noge0822@yahoo.co.jp>
2018-10-26 21:06:42 +0900

    create SCSSfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
@@ -23,6 +24,15 @@ gulp.task('compile', function () {
     .pipe(gulp.dest('js'))
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
@@ -34,11 +44,15 @@ gulp.task('browser-sync', function () {
 gulp.task('watch', function () {
   watch(['./src/js/**/**.js'], function () {
     gulp.start(['compile']);
-  })
+  });
+
+  watch(['./src/sass/**/**.scss'], function () {
+    gulp.start(['sass']);
+  });
 
   watch(['./**/*.html', './js/**/*.js', './css/**/**.css'], function () {
     browserSync.reload();
   });
 });
 
-gulp.task('default', ['compile', 'browser-sync', 'watch']);
+gulp.task('default', ['compile', 'sass', 'browser-sync', 'watch']);
